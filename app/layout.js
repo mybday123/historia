ddf7c78cd8b900d54ac0c4b75318c6fccc74cf6a
Dirga Yuza <dirgayuza123@gmail.com>
2025-08-22 02:28:04 +0700

    Add initial basic UI

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,10 @@
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata = {
   title: "Historia",
@@ -7,7 +13,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
+// import Button from "./components/Button";
+// import UploadUI from "./components/UploadUI";
+
 export default function Home() {
   return (
     <>
       <h1>Historia</h1>
+      {/* <Button icon={"add"}>New Chat</Button>
+      <Button icon={"email"}>Send to Email</Button>
+      <UploadUI />*/}
     </>
   );
 }
