44f58ad6580d2d48a8c5dba4146ed9d475cef537
Dirga Yuza <dirgayuza123@gmail.com>
2025-08-24 16:58:31 +0700

    Added preconnect to material icons
    this should hopefully make the icons load a bit faster

--- app/layout.js
+++ app/layout.js
@@ -15,9 +15,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.className}>
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..400,0..1,-50..200&display=block"
         />
       </head>
       <body>{children}</body>
